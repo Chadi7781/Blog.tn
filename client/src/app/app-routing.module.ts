dbd91ce05a717f5161738a906deef532ef9003ce
shady7781 <troudishedy6@gmail.com>
2021-02-07 13:16:37 +0100

    dashboard/Navbar components & initialisation bootstrap

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {NavbarComponent} from "./components/navbar/navbar.component";
+import {HomeComponent} from "./components/home/home.component";
+import {DashboardComponent} from "./components/dashboard/dashboard.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'',
+    component:HomeComponent
+  },
+  {
+    path:'dashboard',
+    component:DashboardComponent
+  },
+  {path:'**', component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
